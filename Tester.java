785b8d12237932f97959e30cf8c0302dc18522ec
vianalin <vlin21@stuy.edu>
2020-11-22 16:50:54 -0500

    tested getvalue, getnumerator, getdenom

--- Tester.java
+++ Tester.java
@@ -1,19 +1,28 @@
 public class Tester {
 	public static void main (String[]args){
         
-        //RealNumber
-        RealNumber a = new RealNumber(0);
-        RealNumber b = new RealNumber(0);
-        System.out.println(a.equals(b)); //testing equals: true
-        RealNumber c = new RealNumber(499.996);
-        RealNumber d = new RealNumber(500);
-        System.out.println(c.equals(d)); //testing equals: true
-        RealNumber e = new RealNumber(100);
-        System.out.println(d.equals(e)); //testing equals: false
+        //Real Number
+        System.out.println("RealNumber:");
+        RealNumber a1 = new RealNumber(0);
+        RealNumber b1 = new RealNumber(0);
+        System.out.println(a1.equals(b1)); //testing equals: true
+        RealNumber c1 = new RealNumber(499.996);
+        RealNumber d1 = new RealNumber(500);
+        System.out.println(c1.equals(d1)); //testing equals: true
+        RealNumber e1 = new RealNumber(100);
+        System.out.println(d1.equals(e1)); //testing equals: false
 
-        System.out.println(d.add(e)); //testing add
-        System.out.println(d.multiply(e)); //testing multiply
-        System.out.println(d.divide(e)); //testing divide
-        System.out.println(d.subtract(e)); //tesing subtract
+        System.out.println(d1.add(e1)); //testing add
+        System.out.println(d1.multiply(e1)); //testing multiply
+        System.out.println(d1.divide(e1)); //testing divide
+        System.out.println(d1.subtract(e1)); //tesing subtract
+
+        //Rational Number
+        System.out.println("RationalNumber:");
+        RationalNumber a2 = new RationalNumber(-3,8);
+        System.out.println(a2.getValue()); //testing getvalue
+        System.out.println(a2.getNumerator()); //testing getnumerator
+        System.out.println(a2.getDenominator()); //testing getdenominator
+
     }
 }
