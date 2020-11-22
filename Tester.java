54ca831862e232ae19fe7ce629f7b6b34791e761
vianalin <vlin21@stuy.edu>
2020-11-22 17:49:59 -0500

    tested multiply

--- Tester.java
+++ Tester.java
@@ -29,8 +29,10 @@ public class Tester {
         System.out.println(a2.equals(b2)); //testing equals: true
         RationalNumber c2 = new RationalNumber(4,-8);
         System.out.println(b2.equals(c2)); //testing equals: false
-
         System.out.println(c2.toString()); //testing tostring & reduce
+
+        System.out.println(a2.multiply(c2)); //testing multiply
+
 
 
     }
